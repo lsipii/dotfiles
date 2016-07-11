0979395a3e573168f624c3e338b580036a32cc09
Lassi Piironen <Lassi Piironen>
2016-07-11 17:03:18 +0300

    make-links -script

---
+++ make-links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+## Links to home with some path assumptions
+
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
