8bf8b2f73fafeecb699e943a95bc8c7f96e2a5d4
Lassi Piironen <lsipii@Kiihdytin>
2022-08-08 21:07:00 +0300

    fix: permissions, stuff

--- scripts/install-base-configuration-links.sh
+++ scripts/install-base-configuration-links.sh
@@ -4,12 +4,14 @@ SCRIPTSDIR=$(realpath ${0%/*}/..)
 CONFIGURATIONS_DIR="${SCRIPTSDIR}/configurations"
 
 # Links dotfiles of configurations dir to home dir
-for SYMLINK_FILE in ${CONFIGURATIONS_DIR}/.*; do
-do		
+for SYMLINK_FILE in ${CONFIGURATIONS_DIR}/.*; do		
+	echo "Cheking.. ${SYMLINK_FILE}"
 	if [ -f ${CONFIGURATIONS_DIR}/${SYMLINK_FILE} ]; then
 		if [ ! -e ${HOME}/${SYMLINK_FILE} ]; then
 			ln -s ${CONFIGURATIONS_DIR}/${SYMLINK_FILE} ${HOME}/${SYMLINK_FILE} 
 			echo "Made a new link: ${HOME}/${SYMLINK_FILE}"
+		else
+			echo "Link already exists: ${HOME}/${SYMLINK_FILE}"	
 		fi
 	fi
 done
