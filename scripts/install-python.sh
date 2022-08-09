07d77c4d026db7ed28111ffcbcd9e8315d43a5c9
Lassi Piironen <lsipii@kapsi.fi>
2022-08-09 13:26:01 +0300

    fix: add pip

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -2,5 +2,5 @@
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3
+sudo apt install -y python3 python3-pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
