566643af1e4f632e7732c0546f3c1cd4f44ab69c
Lassi Piironen <lsipii@kapsi.fi>
2023-10-05 12:39:45 +0300

    fix: awscli cleanup

--- scripts/install-aws-cli.sh
+++ scripts/install-aws-cli.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 
-mkdir -r awscli
-cd awscli
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-cd ..
-rm -rf awscli
+mkdir -p awscli
+cd awscli && \
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
+unzip awscliv2.zip && \
+sudo ./aws/install --update && \
+cd .. && \
+rm -rf ./awscli
